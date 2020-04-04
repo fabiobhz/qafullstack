4ace9f53fa6103daa1a8a7f774a9f260145d4e13
Fabio Gomes <fabiobhz@gmail.com>
2020-04-04 15:35:36 +0200

    Ruby For testers training material

---
+++ ruby/avancado/excessoes.rb
@@ -0,0 +1,16 @@
+# begin
+#     file = File.open('./ola.txt')
+#     if file
+#         puts file.read
+#     end
+# rescue Exception => e
+#     puts e.backtrace
+# end
+
+def soma(n1,n2)
+    n1 + n2
+rescue Exception => e
+    puts "Erro ao executar a soma"
+end
+
+soma('10', 3)
---
+++ ruby/basico/tipos.rb
@@ -0,0 +1,15 @@
+produto = 'Macbook Air'
+preco = 5999.99
+quantidade = 10
+disponivel = true
+
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
+
+puts produto
+puts quantidade
+puts preco
+puts disponivel
+
